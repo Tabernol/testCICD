918417503bb19e9d8f72302f57fd81d938cc02a6
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2024-02-04 15:06:23 +0200

    delete test line

--- src/main/java/com/example/testcicd/SampleService.java
+++ src/main/java/com/example/testcicd/SampleService.java
@@ -3,8 +3,6 @@ package com.example.testcicd;
 public class SampleService {
 
     public int sampleTestAdd(int a, int b) {
-        System.out.println("test line");
-        System.out.println("test line");
         return a + b;
     }
 }
