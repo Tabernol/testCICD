3d5c41e04c052fef1240c31e5043ccb8d6e8be9d
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2024-02-04 14:45:40 +0200

    Add test a few line

--- src/main/java/com/example/testcicd/SampleService.java
+++ src/main/java/com/example/testcicd/SampleService.java
@@ -3,6 +3,9 @@ package com.example.testcicd;
 public class SampleService {
 
     public int sampleTestAdd(int a, int b) {
+        System.out.println("add a few test line");
+        System.out.println("add a few test line");
+        System.out.println("add a few test line");
         return a + b;
     }
 }
