a52248b153dcb0ad04813ee42391280322edc769
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2024-02-04 15:41:01 +0200

    add new lines

--- src/main/java/com/example/testcicd/SampleService.java
+++ src/main/java/com/example/testcicd/SampleService.java
@@ -4,6 +4,8 @@ public class SampleService {
 
     public int sampleTestAdd(int a, int b) {
         System.out.println("some changes");
+        System.out.println("some changes");
+        System.out.println("some changes");
         return a + b;
     }
 }
