30ecf301c483b280e8cd68ebd7501b4e6c25ac6a
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2024-02-04 16:29:44 +0200

    add new2

--- src/main/java/com/example/testcicd/SampleService.java
+++ src/main/java/com/example/testcicd/SampleService.java
@@ -4,6 +4,7 @@ public class SampleService {
 
     public int sampleTestAdd(int a, int b) {
         System.out.println("test line");
+        System.out.println("t5");
         return a + b;
     }
 }
